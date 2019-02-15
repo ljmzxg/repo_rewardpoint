9ec10d61f030eb92d456658ffdd5f44ac074738a
Sam Zhang <SamZhang@email.com>
2019-02-15 15:53:46 +0800

    springcloud feign

--- tensquare_parent/tensquare_qa/src/main/java/com/iv/tensquare/qa/AppQa.java
+++ tensquare_parent/tensquare_qa/src/main/java/com/iv/tensquare/qa/AppQa.java
@@ -2,7 +2,9 @@ package com.iv.tensquare.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import util.IdWorker;
@@ -10,6 +12,8 @@ import util.JwtUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AppQa {
 	
 	public static void main(String[] args) {
