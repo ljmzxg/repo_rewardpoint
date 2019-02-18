71e492786e71baf07b5eddf4a294f0a772add045
Sam Zhang <SamZhang@email.com>
2019-02-18 14:29:06 +0800

    update module tensquare_friend

---
+++ tensquare_parent/tensquare_friend/src/main/java/com/iv/tensquare/friend/dao/NoFriendDao.java
@@ -0,0 +1,9 @@
+package com.iv.tensquare.friend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iv.tensquare.friend.pojo.NoFriend;
+
+public interface NoFriendDao extends JpaRepository<NoFriend, String> {
+
+}
