2258745477d81b8c041a63cb88682b4dc9a30de5
Sam Zhang <SamZhang@email.com>
2019-02-18 12:06:20 +0800

    update module tensquare_friend

--- tensquare_parent/tensquare_friend/src/main/java/com/iv/tensquare/friend/dao/FriendDao.java
+++ tensquare_parent/tensquare_friend/src/main/java/com/iv/tensquare/friend/dao/FriendDao.java
@@ -1,11 +1,17 @@
 package com.iv.tensquare.friend.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.iv.tensquare.friend.pojo.Friend;
 
 public interface FriendDao extends JpaRepository<Friend, String> {
 
 	public Friend findByUseridAndFriendid(String userid, String friendid);
+
+	@Modifying
+	@Query(value = "update tb_friend set islike = ?1 where userid = ?2 and friendid = ?3 ", nativeQuery = true)
+	public void updateIslike(String islike, String userid, String friendid);
 	
 }
--- tensquare_parent/tensquare_friend/src/main/java/com/iv/tensquare/friend/service/FriendService.java
+++ tensquare_parent/tensquare_friend/src/main/java/com/iv/tensquare/friend/service/FriendService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.iv.tensquare.friend.dao.FriendDao;
+import com.iv.tensquare.friend.pojo.Friend;
 
 @Service
 @Transactional
@@ -16,12 +17,25 @@ public class FriendService {
 	
 	public int addFriend(String userid, String friendid) {
 		//先判断userid 到friendid 是否有数据，有数据表示重复添加，返回0
+		Friend friend = friendDao.findByUseridAndFriendid(userid, friendid);
+		if( friend != null ) {
+			return 0;
+		}
 		
 		//直接添加好友，userid 到 friendid 的type为0
+		friend = new Friend();
+		friend.setUserid(userid);
+		friend.setFriendid(friendid);
+		friend.setIslike("0");
+		friendDao.save(friend);
 		
 		//判断从friend 到 userid 是否有数据，有，将双方的状态都改为“1”
-		
-		return 0;
+		if(friendDao.findByUseridAndFriendid(friendid, userid) != null) {
+			//把双方的islike 都改为1
+			friendDao.updateIslike("1", userid, friendid );
+			friendDao.updateIslike("1", friendid, userid);
+		}
+		return 1;
 	}
 
 }
